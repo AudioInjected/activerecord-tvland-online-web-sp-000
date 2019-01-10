821b4848ce09e132931bc9df94d6929cda980703
Rafael Batista <rafaelb24@yahoo.com>
2019-01-10 18:25:04 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ActiveRecord::Base
   has_many :characters
-  hash_many :shows, through: :shows
+  hash_many :shows, through: :characters
   
   def full_name 
     "#{self.first_name} #{self.last_name}"
