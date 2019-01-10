8e045529162fbb381dcdfcfc899d2814a72c656f
Rafael Batista <rafaelb24@yahoo.com>
2019-01-10 18:36:49 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,4 @@
 class Show < ActiveRecord::Base
   has_many :characters, foreign_key: "character_id"
-  has_many :actors, through: :characters, foreign_key "actor_id"
+  has_many :actors, through: :characters, foreign_key: "actor_id"
 end
