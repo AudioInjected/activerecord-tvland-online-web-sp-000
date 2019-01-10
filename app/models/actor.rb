1cda5c731d51d204c2cf6971260b95d572f5a77a
Rafael Batista <rafaelb24@yahoo.com>
2019-01-10 17:53:46 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,9 +1,3 @@
 class Actor < ActiveRecord::Base
-  has_many characters, through shows
-  def change 
-    create_table :actors do |t|
-      t.string :first_name 
-      t.string :last_name
-    end
-  end 
+
 end
