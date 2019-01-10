83d2d6c48ee3b4e52a385b8ee2043c64591636f5
Rafael Batista <rafaelb24@yahoo.com>
2019-01-10 21:23:56 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_season_to_shows.rb
+++ db/migrate/009_add_season_to_shows.rb
@@ -1,5 +1,5 @@
-class AddDayToShows < ActiveRecord::Migration[4.2] 
+class AddSeasonToShows < ActiveRecord::Migration[4.2] 
   def change 
-    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end 
 end
